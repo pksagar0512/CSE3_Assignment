e0929f244f81971211bbc78c2df0317baac2dc1c
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-02-28 13:32:02 +0530

    added navbar component

--- Assignment/src/app/app.component.ts
+++ Assignment/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule ],
+  imports: [RouterOutlet, RouterModule, NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
