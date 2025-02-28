c9709e62cfe7b0a00dbbd3e9bf5fcc378a0260ff
Bhargavi jaddu <bhanujaddu462@gmail.com>
2025-02-28 13:39:45 +0530

    2303031057131

--- Assignment/src/app/app.routes.ts
+++ Assignment/src/app/app.routes.ts
@@ -1,3 +1,26 @@
+// import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: "home",
+    component:HomeComponent
+},
+{
+    path:"about",
+    component:AboutComponent
+
+},
+{
+    path:"contact",
+    component:ContactComponent
+},
+{
+    path: "services",
+    component:ServicesComponent
+}
+];
