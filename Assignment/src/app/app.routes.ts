825528585ac882f33bbc865c77bb255047d780df
MaredlaAkshitha <maredlaakshitha@gmail.com>
2025-03-01 11:29:15 +0530

    2203031050356

--- Assignment/src/app/app.routes.ts
+++ Assignment/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "home",
+    component: HomeComponent
+    
+},
+{
+    path: "about",
+    component:AboutComponent
+},
+{
+    path: "Contact",
+    component:ContactComponent
+},
+{
+    path:"Services",
+    component:ServicesComponent
+},
+];
