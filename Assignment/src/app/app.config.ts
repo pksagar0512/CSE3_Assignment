ced10c4c15417d11d8c4405ade64fcaf4528aa1c
Manan Chorwadi <mananchorwadi11@gmail.com>
2025-03-01 10:23:43 +0530

    created responsive navigation bar

--- Assignment/src/app/app.config.ts
+++ Assignment/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 import { routes } from './app.routes';
 
--- Assignment/src/app/app.routes.ts
+++ Assignment/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { AboutComponent } from '../components/about/about.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { LoginComponent } from '../components/login/login.component';
+import { LogoutComponent } from '../components/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {
+        path : 'home',
+        component : HomeComponent
+    },
+    {
+        path : 'about',
+        component : AboutComponent
+    },
+    {
+        path : 'contact',
+        component : ContactComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path:'logout',
+        component : LogoutComponent
+    },
+    
+
+
+];
