cd92f39d6346f89c48779a23a06baaa504626abd
Ankit Kumar <ankitkumar@gmail.com>
2025-03-03 11:24:07 +0530

    Assignment-2

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  onLogin() {
+    console.log('Logged in');
+  }
+}
